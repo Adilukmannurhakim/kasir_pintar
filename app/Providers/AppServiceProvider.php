4b3a8e00465af55877686a923e3cfd1ae63e433a
lnstore03 <adilukman665@gmail.com>
2026-07-29 23:29:49 +0700

    Fix HTTPS and session cookie settings

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (config('app.env') !== 'local') {
+        if (config('app.env') !== 'local' || isset($_SERVER['HTTP_X_FORWARDED_PROTO'])) {
         URL::forceScheme('https');
     }
 }
